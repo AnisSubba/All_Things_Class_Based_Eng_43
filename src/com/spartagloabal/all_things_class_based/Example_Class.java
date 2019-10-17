c0419eb6224f130dc475e64f99b1d477708521bf
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-17 11:20:46 +0100

    1st Commit on Class Example which describes the member variable, construct, getter/setter

--- src/com/spartagloabal/all_things_class_based/Example_Class.java
+++ src/com/spartagloabal/all_things_class_based/Example_Class.java
@@ -1,4 +1,35 @@
 package com.spartagloabal.all_things_class_based;
 
 public class Example_Class {
+//    Member Variable
+//    encapsulation is hiding from the user and other class to see or use it
+    private int count;
+    private int newNum = count;
+
+/*    constructor doesn't have 'type'. it doesn't need as it. also no logging
+it will force an input!!! when it has object inside.
+it starts on the initialization
+  */
+
+    public Example_Class(int countNum){
+        this.count = countNum;
+    }
+// getter and setter
+    public int getCount(){
+        return count;
+    }
+//    abstraction = telling people what you need to put in, you need to put in a number
+    public void setCount(int countNum){
+        this.count = countNum;
+    }
+
+//    Methods
+    public void doubleCount(){
+        count = count * 2;
+    }
+
+//    public void newCount(int countNum){
+//        int newNum = countNum;
+//    }
+
 }

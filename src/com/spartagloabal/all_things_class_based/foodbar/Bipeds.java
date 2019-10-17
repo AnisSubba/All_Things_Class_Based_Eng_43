da2cf2026fb1f751f0b3a56aa2cd554c3a7eefb6
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-17 11:27:09 +0100

    1st commit in Bipeds which is extended with Animals

--- src/com/spartagloabal/all_things_class_based/foodbar/Bipeds.java
+++ src/com/spartagloabal/all_things_class_based/foodbar/Bipeds.java
@@ -1,4 +1,8 @@
 package com.spartagloabal.all_things_class_based.foodbar;
 
-public class Bipeds {
+public class Bipeds extends Animal {
+
+    public Bipeds(int numOfLegs, String mouth, int numOfEyes) {
+        super(numOfLegs, mouth, numOfEyes);
+    }
 }
